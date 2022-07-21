b380e9fcaddc41dae44916f9250b4a233190111b
The Papaw <thepapaw@zizzlezazzle.com>
2022-07-21 11:01:39 -0500

    updated script.module.resolveurl

--- script.module.resolveurl/lib/resolveurl/plugins/streamrapid.py
+++ script.module.resolveurl/lib/resolveurl/plugins/streamrapid.py
@@ -19,9 +19,11 @@
 import re
 import json
 import base64
+import random
 from six.moves import urllib_parse
 from resolveurl.lib import helpers
 from resolveurl import common
+from resolveurl.lib import websocket
 from resolveurl.resolver import ResolveUrl, ResolverError
 
 
@@ -47,6 +49,23 @@ class StreamRapidResolver(ResolveUrl):
         number = re.findall(r"recaptchaNumber\s*=\s*'(\d+)", html)
         if token and number:
             eid, media_id = media_id.split('/')
+            sid = ''
+            if eid == '5':
+                try:
+                    ws_servers = ['ws10', 'ws11', 'ws12']
+                    wurl = 'ws://{0}.{1}/socket.io/?EIO=4&transport=websocket'.format(random.choice(ws_servers), host)
+                    ws = websocket.WebSocket()
+                    ws.connect(wurl)
+                    ws.recv()
+                    ws.send("40")
+                    msg = ws.recv()
+                    ws.close()
+                    s = re.search(r'sid":"([^"]+)', msg)
+                    if s:
+                        sid = s.group(1)
+                except:
+                    pass
+
             headers.update({'Referer': web_url, 'Accept': '*/*'})
             surl = '{0}ajax/embed-{1}/getSources'.format(rurl, eid)
             if '?' in media_id:
@@ -54,6 +73,8 @@ class StreamRapidResolver(ResolveUrl):
             data = {'_number': number[0],
                     'id': media_id,
                     '_token': token}
+            if sid:
+                data.update({'sId': sid})
             headers.update({'X-Requested-With': 'XMLHttpRequest'})
             shtml = self.net.http_GET('{0}?{1}'.format(surl, urllib_parse.urlencode(data)), headers=headers).content
             sources = json.loads(shtml).get('sources')
